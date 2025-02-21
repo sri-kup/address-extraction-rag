6229b8faf30b1796f46857bb5503b5471d3541af
sri-kup <srikup20@gmail.com>
2025-02-21 11:12:33 +0000

    Add embeddings and LangChain integration to vector store module

--- vector_store_chroma.py
+++ vector_store_chroma.py
@@ -1,5 +1,10 @@
 import chromadb
 import uuid
+from langchain_chroma import Chroma
+from langchain_huggingface import HuggingFaceEmbeddings
+from chromadb.utils.embedding_functions import create_langchain_embedding
+
+embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
 
 def get_chroma_client(persist_directory="chroma_db"):
     """
@@ -12,7 +17,8 @@ def create_collection(client, collection_name="default_collection"):
     """
     Creates or retrieves a collection in Chroma.
     """
-    return client.get_or_create_collection(name=collection_name)
+    ef = create_langchain_embedding(embeddings)
+    return client.get_or_create_collection(name=collection_name, embedding_function=ef)
 
 def upsert_chunks(collection, chunks):
     """
